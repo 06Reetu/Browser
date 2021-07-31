e3079fe063e6e2343849af82e272903ea4afbc9c
Reetu <73978459+06Reetu@users.noreply.github.com>
2021-07-31 20:29:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtGui import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+
+        #HomePage
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.initUI()
+        self.browser.setUrl(QUrl('http://google.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+        # Navigation
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction('Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        forward_btn = QAction('Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('http://google.com'))
+
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+    def initUI(self):
+        self.setGeometry(300,300,300,220)
+        self.setWindowTitle('Alpha Browser')
+        self.setWindowIcon(QIcon('browser.png'))
+
+        self.show()
+
+
+app = QApplication(sys.argv)
+trayIcon = QSystemTrayIcon(QIcon('browser.png'), parent= app)
+trayIcon.show()
+window = MainWindow()
+app.exec_()
